84114bee9db27261914269a555adbe34b20ed93d
JacksonByrd7 <jackson.byrd7@gmail.com>
2024-04-06 20:26:17 -0400

    created mysql connection

--- config/init.js
+++ config/init.js
@@ -4,7 +4,12 @@ require('dotenv').config();
 (async () => {
   try {
     const { DB_NAME } = process.env;
-    const db = await mysql.createConnection('mysql://root@127.0.0.1:3306');
+    const db = await mysql.createConnection({
+      host: 'localhost',
+      port: '3306',
+      user: 'root',
+      password: '', // Add your password here if you have one
+    });
     if (process.argv[2] === 'seed') {
       await db.execute(`DROP DATABASE IF EXISTS ${DB_NAME}`);
       console.log(`${DB_NAME} dropped`);
